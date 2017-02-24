e3dbbeed9d727a68f42c81bb1956f986e6c6431d
Robert Prater <rprater@Roberts-MacBook-Air.local>
2017-02-23 19:24:20 -0500

    Initial Commit

---
+++ Simple Git Server Test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Simple Git Server Test
+//
+//  Created by Robert Prater on 2017-02-23.
+//  Copyright © 2017 Spectre. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
